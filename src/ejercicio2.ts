b3e3342e3a1d8050f2d042f9c1e942a82d9a2571
Kevin <kevinchanchi02@gmail.com>
2024-10-23 21:24:05 -0500

    2.1

---
+++ src/ejercicio2.ts
@@ -0,0 +1,6 @@
+const numbers: number[] = [1, 2, 3, 4, 5];
+const fruits: string[] = ['Manzana', 'Pera', 'Kiwi', 'Piña'];
+const booleans: boolean[] = [true, false, false, true];
+// tupla
+
+numbers.push(6);
